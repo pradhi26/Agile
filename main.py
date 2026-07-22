1243bc151a38fe17fd75ae3791303a257120b97e
Pradhiksha G <gpradhi26@gmail.com>
2026-07-22 22:19:58 +0530

    Final working

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from login import LoginPage
+
+
+class TouristBookingSystem:
+
+    def __init__(self):
+
+        # Start login page
+        LoginPage()
+
+
+
+if __name__ == "__main__":
+
+    app = TouristBookingSystem()
